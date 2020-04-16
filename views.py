420ee996958cee0992032f605f54dd8060e43eb4
anutamminen <62876094+anutamminen@users.noreply.github.com>
2020-04-16 11:27:32 +0300

    API:sta saatiedot saava saapalvelu

--- views.py
+++ views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 import requests
 from .models import City
 from .forms import CityForm
+from pyowm import OWM
+import json
 
 # Create your views here.
+
+API_key = ''
+owm = OWM(API_key)
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='
@@ -38,3 +43,27 @@ def index(request):
 
     context = {'weather_data' : weather_data, 'form' : form}
     return render(request, 'weather/weather.html', context)
+
+def visualization(request):
+
+    cities = City.objects.all()
+    temp_data = []
+
+    for city in cities:
+        fc = owm.three_hours_forecast(city.name)
+        f = fc.get_forecast()
+
+        temp_list = list()
+        for weather in f:
+                temp = weather.get_temperature(unit='celsius')['temp']
+                temp_list.append(temp)
+
+        forecast = {
+            'name' : city.name,
+            'data' : temp_list
+        }   
+
+        temp_data.append(forecast)
+
+    context = {'temp_data'  : json.dumps(temp_data)}
+    return render(request, 'weather/visualization.html', context)
